8cb02ffad4bbd5bc780d50c0328c2a25f95ee408
Andrii Pyrih <andrii.pyrih@gmail.com>
2024-10-01 21:48:30 +0100

    (refactor): Add logging

--- dashboard/update_page_with_activities.py
+++ dashboard/update_page_with_activities.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from stravalib import Client
@@ -6,12 +7,18 @@ from strava_activity_fetcher import StravaActivityFetcher
 from strava_authenticator import StravaAuthenticator
 from strava_service import StravaService
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
 if __name__ == "__main__":
     # Load Strava credentials from environment variables
     client_id = os.getenv("STRAVA_CLIENT_ID")
     client_secret = os.getenv("STRAVA_CLIENT_SECRET")
     refresh_token = os.getenv("STRAVA_REFRESH_TOKEN")
 
+    logging.info("Starting Strava API client setup...")
     client = Client()
     authenticator = StravaAuthenticator(client_id, client_secret, refresh_token, client)
     fetcher = StravaActivityFetcher(client)
